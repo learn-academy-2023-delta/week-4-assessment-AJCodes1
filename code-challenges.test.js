e0f62a4b50894c48513da3803af347db21ac4711
AJCodes1 <etsncamacho@gmail.com>
2023-06-04 22:16:59 -0700

    week 4 assesment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,44 +14,90 @@
 
 // a) Create a test with an expect statement using the variable provided.
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
+describe("removeFirstShuffle", () => {
+  const expected = ["yellow", "blue", "pink", "green"]
+  const expected2 = ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+  it("removes the first item from the array and shuffles the remaining content", () => {
+    const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+    // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+    const colors2 = [
+      "chartreuse",
+      "indigo",
+      "periwinkle",
+      "ochre",
+      "aquamarine",
+      "saffron"
+    ]
+    // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+    expect(removeFirstShuffle(colors1).toEqual(expect.arrayContaining(expected)))
+    expect(removeFirstShuffle(colors2).toEqual(expect.arrayContaining(expected2)))
+  })
+})
+// // b) Create the function that makes the test pass.
 
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
-// Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = [
-  "chartreuse",
-  "indigo",
-  "periwinkle",
-  "ochre",
-  "aquamarine",
-  "saffron"
-]
-// Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+// // Pseudo code:
+// // create a function that takes in an array 
+const removeFirstShuffle = (array) => {          //create a function that takes in an array 
+  array.shift()                                  // removes the first element 
+  return array.sort(() => Math.random() - 0.5);  // shuffle the array
+}
 
-// b) Create the function that makes the test pass.
 
-// Pseudo code:
+
+
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
 // a) Create a test with expect statements for each of the variables provided.
+describe("tallyCount", () => {
+  it("takes in an object that contains up votes and down votes and returns the end tally", () => {
+    const votes1 = { upVotes: 13, downVotes: 2 }
+    // Expected output: 11
+    const votes2 = { upVotes: 2, downVotes: 33 }
+    // Expected output: -31
+    expect(tallyCount(votes1)).toEqual((11))  
+    expect(tallyCount(votes2)).toEqual((-31))
+  })
+}) 
+// // b) Create the function that makes the test pass.
 
-const votes1 = { upVotes: 13, downVotes: 2 }
-// Expected output: 11
-const votes2 = { upVotes: 2, downVotes: 33 }
-// Expected output: -31
+// // Pseudo code:
+//create a fucntion named tallyCount that takes in count
+const tallyCount = (count) => {
+//return .upVotes - .downVotes to get the total votes 
+  return count.upVotes - count.downVotes
+}
 
-// b) Create the function that makes the test pass.
+// // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
-// Pseudo code:
+// // a) Create a test with an expect statement using the variables provided.
 
-// --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
+describe("noDuplicateValue", () => {
+  it("returns one array with no duplicate values.", () => {
 
-// a) Create a test with an expect statement using the variables provided.
+    const dataTypey1sArra = ["array", "object", "number", "string", "Boolean"]
+    const dataTypesArray2 = ["string", "null", "Boolean", "string", "undefined"]
+    // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
 
-const dataTypesArray1 = ["array", "object", "number", "string", "Boolean"]
-const dataTypesArray2 = ["string", "null", "Boolean", "string", "undefined"]
-// Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+expect(noDuplicateValue(dataTypey1sArra, dataTypesArray2)).toEqual((["array", "object", "number", "string", "Boolean", "null", "undefined"]))
 
-// b) Create the function that makes the test pass.
+  })
+}) 
+// // // b) Create the function that makes the test pass.
 
-// Pseudo code:
+// // // Pseudo code:
+
+const noDuplicateValue = (array1, array2) => { // create a funtion that takes 2 arrays
+  let newArray = []                            // create an empty array
+  for(let i = 0; i < array1.length; i++) {     //iterate first array
+    if(newArray.indexOf(array1[i]) === -1) {   // check if the element is present in newArray
+      newArray.push(array1[i])                  // push to newArray 
+    }
+  }
+  for(let i = 0; i < array2.length; i++) {      //iterate second array 
+    if(newArray.indexOf(array2[i]) === -1) {    // check if element is present in newArray 
+      newArray.push(array2[i])                  // push to newArray
+    }
+  }
+  return newArray
+}
